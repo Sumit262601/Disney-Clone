a1392b9c8f9fb8dbdc19c23803fa8a8dfc2553d5
Sumit-Dev <127303989+Sumit262601@users.noreply.github.com>
2024-06-23 19:08:56 +0530

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,7 +3,7 @@ import GlobalApi from '../Services/GlobalApi'
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 
 
-const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+const IMAGE_BASE_URL = "MOVIE_API_KEY";
 const screenWidth=window.innerWidth;
 
 function Slider() {
